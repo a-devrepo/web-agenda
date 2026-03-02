e10dd98ecaed14fbc40403ed94d753952160dc4c
alison <adevpr11@gmail.com>
2026-03-02 10:38:14 -0300

    feat: adiciona rotas para os componentes de cadastro e autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AutenticarUsuario } from './pages/autenticar-usuario/autenticar-usuario';
+import { CadastrarUsuario } from './pages/cadastrar-usuario/cadastrar-usuario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pages/autenticar-usuario',
+    component: AutenticarUsuario,
+  },
+  {
+    path: 'pages/cadastrar-usuario',
+    component: CadastrarUsuario,
+  },
+  {
+    path: '',
+    redirectTo: 'pages/autenticar-usuario', pathMatch: 'full',
+  } 
+];
