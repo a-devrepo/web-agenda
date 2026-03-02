f1da28fed9b3a8a67a038f3d33b11a46ffc1f995
alison <adevpr11@gmail.com>
2026-03-02 11:16:05 -0300

    feat: implmenta interface no componente de cadastro

--- src/app/pages/cadastrar-usuario/cadastrar-usuario.ts
+++ src/app/pages/cadastrar-usuario/cadastrar-usuario.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-usuario',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './cadastrar-usuario.html',
   styleUrl: './cadastrar-usuario.css',
 })
