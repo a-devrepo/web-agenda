76e3ccc333015d2af985e098fe08af10a4714e3d
alison <adevpr11@gmail.com>
2026-03-02 11:15:27 -0300

    feat: implmenta interface no componente de autenticação

--- src/app/pages/autenticar-usuario/autenticar-usuario.ts
+++ src/app/pages/autenticar-usuario/autenticar-usuario.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-autenticar-usuario',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './autenticar-usuario.html',
   styleUrl: './autenticar-usuario.css',
 })
