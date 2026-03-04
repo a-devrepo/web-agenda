52c0fc632c97c9233dbee2b44627e8b4d5190fff
alison <adevpr11@gmail.com>
2026-03-04 15:01:48 -0300

    feat: implementa autenticação de usuário

--- src/app/pages/autenticar-usuario/autenticar-usuario.ts
+++ src/app/pages/autenticar-usuario/autenticar-usuario.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-autenticar-usuario',
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './autenticar-usuario.html',
   styleUrl: './autenticar-usuario.css',
 })
-export class AutenticarUsuario {}
+export class AutenticarUsuario {
+
+  httpClient = inject(HttpClient);
+
+  mensagemErro = signal<string>('');
+
+  baseUrl = 'http://localhost:8081/api/v1/usuarios';
+
+  formulario = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required]),
+  });
+
+
+  autenticar(){
+    if(this.formulario.invalid){
+      return;
+    }
+
+    this.limparMensagens();
+
+    const usuario = this.formulario.getRawValue();
+
+    this.httpClient.post(this.baseUrl+'/autenticar', usuario,{responseType: 'text'})
+    .subscribe({
+      next: (res:any) => {
+
+        this.formulario.reset();
+      },
+      error: (err:any) => {
+
+        this.mensagemErro.set(err.error);
+      }
+    });
+  }
+
+  private limparMensagens(){
+    this.mensagemErro.set('');
+  }
+}
