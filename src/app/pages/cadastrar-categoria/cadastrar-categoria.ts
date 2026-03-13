aa84788126981526737437342197236201433915
alison <adevpr11@gmail.com>
2026-03-13 10:11:24 -0300

    feat: adiciona componentes para cadastrar, editar e consultar categorias

---
+++ src/app/pages/cadastrar-categoria/cadastrar-categoria.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-categoria',
+  imports: [],
+  templateUrl: './cadastrar-categoria.html',
+  styleUrl: './cadastrar-categoria.css',
+})
+export class CadastrarCategoria {}
