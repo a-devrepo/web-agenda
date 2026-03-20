e1e95667bf8ff882018d67538e9ceff68f0932a4
alison <adevpr11@gmail.com>
2026-03-20 11:35:16 -0300

    feat: adiciona componentes da entidade tarefa

---
+++ src/app/pages/cadastrar-tarefa/cadastrar-tarefa.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-tarefa',
+  imports: [],
+  templateUrl: './cadastrar-tarefa.html',
+  styleUrl: './cadastrar-tarefa.css',
+})
+export class CadastrarTarefa {}
